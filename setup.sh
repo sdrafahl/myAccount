6a440a2e398e6b1c8b27c8247000a609457a7e85
Shane P Drafahl <sdrafahl@iastate.edu>
2016-11-27 19:12:32 -0600

    Made setup file more in depth

--- setup.sh
+++ setup.sh
@@ -1,9 +1,19 @@
 apt-get install libmysqlcppconn-dev
-echo "Please Enter Your MySQL Username or if You Dont Have a Username Please Reread README.md"
+
+echo "Create New MYSQL USER: y,n"
+read new
+echo "Please Enter Either New User or Existing User"
 read user
-mysql -p -u "$user" accountDB < layout.dump
-
-
-# note database is accountDB , user is shane, table ACCOUNTS and table TABLE_<ACCOUNT>
+echo "Please Enter Either New Password or Existing Password"
+read password
+if ["$new" = "y"]; then
+    echo "Creating New User at localhost"
+    mysql -e "CREATE USER ${user}@localhost IDENTIFIED BY '${password}';"
+fi
+mysql -e -p"$password" CREATE DATABASE accountDB
+mysql -e -p"$password" GRANT ALL PRIVILEGES ON ${user}.* TO '${user}'@'localhost'
+mysql -e -p"$password" FLUSH PRIVILEGES;
+mysql -u "$user" accountDB < layout.sql
+# note database is accountDB , table ACCOUNTS and table TABLE_<ACCOUNT>
 
 
