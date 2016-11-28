0c0b5f34594d9688fbe548d22acf4a2f8ee117fa
Shane P Drafahl <sdrafahl@iastate.edu>
2016-11-27 18:13:22 -0600

    setup dump file for mysql

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+apt-get install libmysqlcppconn-dev
 echo "Please Enter Your MySQL Username or if You Dont Have a Username Please Reread README.md"
 read user
 mysql -p -u "$user" accountDB < layout.dump
